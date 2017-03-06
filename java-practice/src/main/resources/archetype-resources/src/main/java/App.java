6bf48a70f9a1679685c3db24f20466fc23994177
Owner <Owner@Owner-PC>
2017-03-06 11:20:27 +0530

    Added sysout

--- java-practice/src/main/resources/archetype-resources/src/main/java/App.java
+++ java-practice/src/main/resources/archetype-resources/src/main/java/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Worldsdewqew!" );
+        System.out.println( "going to revert this!" );
     }
 }
